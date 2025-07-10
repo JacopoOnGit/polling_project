cc6164a87fbb4eb39ffc57f48d1acfd593092e92
jacopocutrignelli <jacopo.cutrignelli@edu.unifi.it>
2025-07-10 21:48:37 +0200

    progetto completato, variabili d'ambiente per SECRET_KEY, DEBUG, ALLOWED_HOSTS

--- polling_api/settings.py
+++ polling_api/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "sviluppo-non-sicuro")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 

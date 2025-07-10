d74b1c031c1059e451ab2f5c1e731ca1a077295c
jacopocutrignelli <jacopo.cutrignelli@edu.unifi.it>
2025-07-10 23:16:53 +0200

    progetto completato, variabili d'ambiente per SECRET_KEY, DEBUG, ALLOWED_HOSTS

--- polling_api/settings.py
+++ polling_api/settings.py
@@ -47,9 +47,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -143,10 +144,8 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'https://localhost:5173',
-    "https://jacopoongit.github.io",
-]
+CORS_ALLOWED_ORIGINS = True
+CORS_ALLOW_HEADERS = ["*"]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [

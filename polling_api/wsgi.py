b3d95f7a5233159f41c4e817910f15a6b847becc
jacopocutrignelli <jacopo.cutrignelli@edu.unifi.it>
2025-07-11 13:07:28 +0200

    progetto completato, variabili d'ambiente per SECRET_KEY, DEBUG, ALLOWED_HOSTS

--- polling_api/wsgi.py
+++ polling_api/wsgi.py
@@ -1,22 +1,12 @@
-"""
-WSGI config for polling_api project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polling_api.settings')
 
-from django.core.wsgi import get_wsgi_application
-# ESEGUE MIGRAZIONI AUTOMATICHE IN PRODUZIONE
 import django
 django.setup()
+
 from django.core.management import call_command
 call_command('migrate')
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polling_api.settings')
-
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+

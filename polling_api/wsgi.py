1d2e71a094ed2a2a6426f3df68c3bd174c05016d
jacopocutrignelli <jacopo.cutrignelli@edu.unifi.it>
2025-07-11 13:01:40 +0200

    progetto completato, variabili d'ambiente per SECRET_KEY, DEBUG, ALLOWED_HOSTS

--- polling_api/wsgi.py
+++ polling_api/wsgi.py
@@ -10,6 +10,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+# ESEGUE MIGRAZIONI AUTOMATICHE IN PRODUZIONE
+import django
+django.setup()
+from django.core.management import call_command
+call_command('migrate')
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polling_api.settings')
 

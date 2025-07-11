5685d3941fcfbac2a4e9c670a9c7506fc1e5af2e
jacopocutrignelli <jacopo.cutrignelli@edu.unifi.it>
2025-07-11 12:15:29 +0200

    progetto completato, variabili d'ambiente per SECRET_KEY, DEBUG, ALLOWED_HOSTS

--- accounts/views.py
+++ accounts/views.py
@@ -1,30 +1,13 @@
 from rest_framework import generics
-from django.contrib.auth import get_user_model
 from rest_framework.permissions import AllowAny
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
 
 User = get_user_model()
-
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'password', 'bio']
-        extra_kwargs = {
-            'password': {'write_only': True},
-            'bio': {'required': False}
-        }
-
-    def create(self, validated_data):
-        validated_data.pop('is_staff', None)
-        validated_data.pop('is_superuser', None)
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
 
